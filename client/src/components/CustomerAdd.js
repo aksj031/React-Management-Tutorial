85d7a4e55e0542c8c7ab289f298af5ec556d32ef
aksj031 <twin981106a@gmail.com>
2022-09-14 16:38:31 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { post } from 'axios';
+import { Dialog , DialogActions, DialogTitle, DialogContent} from '@mui/material';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import { withStyles } from '@mui/styles';
+import { useTheme } from '@mui/styles';
+
+const styles = useTheme => ({
+    hidden: {
+        display: 'none'
+    }
+})
 
 class CustomerAdd extends React.Component{
     constructor(props){
@@ -10,7 +21,8 @@ class CustomerAdd extends React.Component{
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     }
 
@@ -27,7 +39,8 @@ class CustomerAdd extends React.Component{
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
     }
 
@@ -60,19 +73,53 @@ class CustomerAdd extends React.Component{
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
+    }
     render(){
+        const { classes } = this.props;
         return(
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가하기</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/><br/>
+                        <TextField variant="standard" label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+                        <TextField variant="standard" label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                        <TextField variant="standard" label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                        <TextField variant="standard" label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                        <DialogActions>
+                            <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                            <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                        </DialogActions>
+                    </DialogContent>
+                </Dialog>
+            </div>
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
